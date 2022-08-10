a905a3fe92c45c3ba68f389f7d4487a88023c7ba
Dağhan Günhan <104035382+daghangunhan@users.noreply.github.com>
2022-08-10 15:39:43 +0300

    First Commit

---
+++ proje_doc.py
@@ -0,0 +1,13 @@
+#a=input("Enter a list which you want to flatten:\n")
+l=[[1,'a',['cat'],2],[[[3]],'dog'],4,5]
+i=0
+while i<len(l):
+    if type(l[i])==list:
+        a=l[i]
+        l.pop(i)
+        j=0
+        while j<len(a):
+            l.insert(i+j,a[j])
+            j=j+1
+    else: i=i+1
+print(l)
